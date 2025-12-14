e03e519758b19e68768e1b2d76ae3a0d2fec18b4
Gyong97 <999kgy@naver.com>
2025-12-14 17:38:57 +0900

    Feat : edit struct.

--- model/todo.go
+++ model/todo.go
@@ -1,14 +1,19 @@
 package model
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Todo struct {
-	// gorm.Model을 상속받으면 ID, CreatedAt, UpdatedAt, DeletedAt이 자동 포함
-	gorm.Model
+	// gorm.Model 대신 필요한 것만 직접 정의합니다.
+	// `gorm:"primaryKey"` 태그로 이게 PK임을 알려줍니다.
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`     // 생성 시간
+	UpdatedAt time.Time      `json:"-"`              // 수정 시간 (JSON에는 숨김)
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 삭제 시간 (Soft Delete용, JSON 숨김)
 
-	ID   int    `json:"id"`
 	Task string `json:"task"`
 	Done bool   `json:"done"`
 }
